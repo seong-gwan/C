4aa09a8f9eb5e923a8445819d59ead923a59bfed
seong-gwan <gary261@naver.com>
2016-12-16 07:56:28 +0900

    my suggeest
    I just wonder it compiles.  so I changed a little bit for compiling.

--- 经典示例/010.猜数字游戏/10.c
+++ 经典示例/010.猜数字游戏/10.c
@@ -21,38 +21,43 @@ void main()
 			puts("Please input a number between 1 and 100: ");
 			scanf("%d",&Number);
 			printf("Your input number is %d\n",Number);
-		}while( !(Number>=1 && Number<=100) );
-		if( Number >= 90 )
+		}while( Number>=1 && Number<=100 ) //I don't think this is necessary
 		{
-			printf("Too Bigger! Press any key to try again!\n");
-		}
-		else if( Number >= 70 && Number < 90 )
-		{
-			printf("Bigger!\n");
-		}
-		else if( Number >= 1 && Number <= 30 )
-		{
-			printf("Too Small! Press any key to try again!\n");
-		}
-		else if( Number > 30 && Number <= 50 )
-		{
-			printf("Small! Press any key to try again!\n");
-		}
-		else
-		{
-			if( Number == price )
+			if( Number >= 90 )
 			{
-				printf("OK! You are right! Bye Bye!\n");
+				printf("Too Bigger! Press any key to try again!\n");
 			}
-			else if( Number < price )
+			else if( Number >= 70 && Number < 90 )
 			{
-				printf("Sorry,Only a little smaller! Press any key to try again!\n");
+				printf("Bigger!\n");
+			}
+			else if( Number >= 1 && Number <= 30 )
+			{
+				printf("Too Small! Press any key to try again!\n");
+			}
+			else if( Number > 30 && Number <= 50 )
+			{
+				printf("Small! Press any key to try again!\n");
+			}
+			else
+			{
+				if( Number == price )
+				{
+					printf("OK! You are right! Bye Bye!\n");
+					break;
+				}
+				else if( Number < price )
+				{
+					printf("Sorry,Only a little smaller! Press any key to try again!\n");
 
+				}
+				else if( Number > price )
+				{
+					printf(" Sorry, Only a little bigger! Press any key to try again!\n");
+				}
 			}
-			else if( Number > price )
-				printf(" Sorry, Only a little bigger! Press any key to try again!\n");
+			getch();
 		}
-		getch();
 	}
 }
 
